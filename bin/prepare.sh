ab7e5e6f6f8a8501e99dc5dbf93456c503c202f4
i.nadelyaev <i.nadelyaev@rambler-co.ru>
2016-03-09 13:36:17 +0300

    fix

---
+++ bin/prepare.sh
@@ -0,0 +1,18 @@
+mkdir ~/.local/
+mkdir ~/.local/bin
+
+git clone https://github.com/ilyanadelyaev/_profile.git ~/.local/.profile
+rm -rf ~/.local/.profile/.git
+
+ln -sf ~/.local/.profile/.bash_local.rambler ~/.bash_local
+ln -sf ~/.local/.profile/.bash_profile ~/.bash_profile
+ln -sf ~/.local/.profile/.bashrc ~/.bashrc
+ln -sf ~/.local/.profile/.git-completion.bash ~/.git-completion.bash
+ln -sf ~/.local/.profile/.grab_ssh_agent ~/.grab_ssh_agent
+ln -sf ~/.local/.profile/.inputrc ~/.inputrc
+ln -sf ~/.local/.profile/.python_startup ~/.python_startup
+ln -sf ~/.local/.profile/.screenrc ~/.screenrc
+ln -sf ~/.local/.profile/.vim/ ~/.vim/
+ln -sf ~/.local/.profile/.vimrc ~/.vimrc
+
+ln -sf ~/.local/.profile/bin/add_rsa_keys ~/.local/bin/add_rsa_keys
