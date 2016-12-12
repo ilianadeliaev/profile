b579d7f8d3b45cb8557bacd8907829ec38815272
i.nadelyaev <i.nadelyaev@rambler-co.ru>
2016-12-12 11:47:35 +0300

    up

--- bin/ssh_go.sh
+++ bin/ssh_go.sh
@@ -1,24 +1,38 @@
 #!/usr/bin/env bash
 
 MY=(
+    # '10.41.41.242'
     'nadelyaev.dev.crm.rambler-co.ru'
-    'nadelyaev.dev.crm.rambler.ru'
 )
 
 DEV=(
     'back01.dev.crm.rambler-co.ru'
     'queue01.dev.crm.rambler-co.ru'
-    'front01.dev.crm.rambler-co.ru'
     'subs01.dev.crm.rambler-co.ru'
     'storage01.dev.crm.rambler-co.ru'
+    'front01.dev.crm.rambler-co.ru'
+    'nginx01.dev.crm.rambler-co.ru'
+)
+
+QA=(
+    'back01.qa.crm.rambler-co.ru'
+    'back02.qa.crm.rambler-co.ru'
+    'queue01.qa.crm.rambler-co.ru'
+    'queue02.qa.crm.rambler-co.ru'
+    'subs01.qa.crm.rambler-co.ru'
+    'subs02.qa.crm.rambler-co.ru'
+    'storage01.qa.crm.rambler-co.ru'
+    'storage02.qa.crm.rambler-co.ru'
+    'front01.qa.crm.rambler-co.ru'
+    'front02.qa.crm.rambler-co.ru'
 )
 
 STAGE=(
     'back01.stage.crm.rambler-co.ru'
     'queue01.stage.crm.rambler-co.ru'
-    'front01.stage.crm.rambler-co.ru'
     'subs01.stage.crm.rambler-co.ru'
     'storage01.stage.crm.rambler-co.ru'
+    'front01.stage.crm.rambler-co.ru'
 )
 
 PROD=(
@@ -26,26 +40,22 @@ PROD=(
     'back02.crm.rambler-co.ru'
     'queue01.crm.rambler-co.ru'
     'queue02.crm.rambler-co.ru'
-    'front01.crm.rambler-co.ru'
-    'front02.crm.rambler-co.ru'
     'subs01.crm.rambler-co.ru'
     'subs02.crm.rambler-co.ru'
     'storage01.crm.rambler-co.ru'
     'storage02.crm.rambler-co.ru'
-)
-
-QA=(
-    'back01.dev.crm.rambler-co.ru'
-    'queue01.dev.crm.rambler-co.ru'
-    'front01.dev.crm.rambler-co.ru'
-    'subs01.dev.crm.rambler-co.ru'
-    'storage01.dev.crm.rambler-co.ru'
+    'front01.crm.rambler-co.ru'
+    'front02.crm.rambler-co.ru'
 )
 
 MISC=(
+    'build01.crm.rambler-co.ru'
+    'ci01.crm.rambler-co.ru'
+    'ci02.crm.rambler-co.ru'
+    'ci03.crm.rambler-co.ru'
+    'ci04.crm.rambler-co.ru'
+    'ci05.crm.rambler-co.ru'
     'mailer.dev.crm.rambler-co.ru'
-    'repo.crm.rambler-co.ru'
-    'repo02.crm.rambler-co.ru'
 )
 
 
@@ -63,6 +73,12 @@ if [ -z "$1" ]; then
         printf "1%d: %s\n" $i ${DEV[$i]}
     done
 
+    echo '- QA -'
+    for i in ${!QA[*]}
+    do
+        printf "4%d: %s\n" $i ${QA[$i]}
+    done
+
     echo '- STAGE -'
     for i in ${!STAGE[*]}
     do
@@ -75,16 +91,10 @@ if [ -z "$1" ]; then
         printf "3%d: %s\n" $i ${PROD[$i]}
     done
 
-    echo '- QA -'
-    for i in ${!QA[*]}
-    do
-        printf "4%d: %s\n" $i ${QA[$i]}
-    done
-
     echo '-- MISC --'
     for i in ${!MISC[*]}
     do
-        printf "5%d: %s\n" $i ${MISC[$i]}
+        printf "9%d: %s\n" $i ${MISC[$i]}
     done
 
     echo 'CHOICE [with ENTER]:'
@@ -109,27 +119,27 @@ if (( $choice >= 10 && $choice <= 19 )); then
     server=${DEV[${choice}]}
 fi
 
-# STAGE
+# QA
 if (( $choice >= 20 && $choice <= 29 )); then
     ((choice = choice - 20))
+    server=${QA[${choice}]}
+fi
+
+# STAGE
+if (( $choice >= 30 && $choice <= 39 )); then
+    ((choice = choice - 30))
     server=${STAGE[${choice}]}
 fi
 
 # PROD
-if (( $choice >= 30 && $choice <= 39 )); then
-    ((choice = choice - 30))
+if (( $choice >= 40 && $choice <= 49 )); then
+    ((choice = choice - 40))
     server=${PROD[${choice}]}
 fi
 
-# QA
-if (( $choice >= 40 && $choice <= 49 )); then
-    ((choice = choice - 40))
-    server=${QA[${choice}]}
-fi
-
 # MISC
-if (( $choice >= 50 && $choice <= 59 )); then
-    ((choice = choice - 50))
+if (( $choice >= 90 && $choice <= 99 )); then
+    ((choice = choice - 90))
     server=${MISC[${choice}]}
 fi
 
